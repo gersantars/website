b55c1837b037d3d4ac578f6db99496f35ded0539
gersantars-web <gersantars@gmail.com>
2026-02-19 16:50:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 const filter = document.getElementById("categoryFilter");
 const rows = document.querySelectorAll(".project-row");
-alert("JS cargado");
 
-filter.addEventListener("change", () => {
-  rows.forEach(row => {
-    row.style.display =
-      filter.value === "all" || row.dataset.category === filter.value
-        ? "grid"
-        : "none";
+if (filter) {
+  filter.addEventListener("change", () => {
+    rows.forEach(row => {
+      row.style.display =
+        filter.value === "all" || row.dataset.category === filter.value
+          ? "grid"
+          : "none";
+    });
   });
-});
+}
 
 rows.forEach(row => {
-  row.querySelector(".expand").addEventListener("click", () => {
-    const details = row.querySelector(".project-details");
-    details.style.display =
-      details.style.display === "block" ? "none" : "block";
-  });
+  const expand = row.querySelector(".expand");
+  if (expand) {
+    expand.addEventListener("click", () => {
+      const details = row.querySelector(".project-details");
+      details.style.display =
+        details.style.display === "block" ? "none" : "block";
+    });
+  }
 });
-
